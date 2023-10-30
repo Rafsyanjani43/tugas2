002240e4f75644ed20ff50977c299be8eb107b76
Rafsyanjani43 <rafsyanjani43@gmail.com>
2023-10-30 09:16:36 +0700

    praktikum modul 3

---
+++ src/PersegiPanjang.java
@@ -0,0 +1,17 @@
+
+public class PersegiPanjang {
+
+    public static double hitungLuas(double panjang, double lebar) {
+        return panjang * lebar;
+    }
+
+    public static void main(String[] args) {
+        double panjang = 5.0;
+        double lebar = 3.0;
+
+        double luas = hitungLuas(panjang, lebar);
+
+        System.out.println("Luas dari persegi panjang dengan panjang " + panjang +
+                " dan lebar " + lebar + " adalah " + luas);
+    }
+}
